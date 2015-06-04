f2dfc61ed311f4cdd447bc30ad6ed901ded8c03b
Epsichaos <constant.camille.chaos@gmail.com>
2015-06-04 13:21:30 +0100

    login system

---
+++ js/app.js
@@ -0,0 +1,71 @@
+﻿'use strict';
+
+//ajax cache
+$.ajaxSetup({ cache: false });
+
+// declare modules
+angular.module('Authentication', []);
+
+angular.module('WebApp', [
+    'Authentication',
+    'ngRoute',
+    'ngCookies'
+])
+
+.config(['$routeProvider', function ($routeProvider) {
+
+    $routeProvider
+        .when('/login', {
+            controller: 'LoginController',
+            templateUrl: 'partials/login.html'
+        })
+
+        .when('/', {
+            templateUrl: 'partials/home.html'
+        })
+
+        .when('/historic', {
+            templateUrl: 'partials/historic.html'
+        })
+
+        .when('/comparisons', {
+            templateUrl: 'partials/comparisons.html'  
+        })
+
+        .when('/tips', {
+            templateUrl: 'partials/tips.html'
+        })
+
+        .when('/social', {
+            templateUrl: 'partials/social.html'
+        })
+
+        .when('/contact', {
+            templateUrl: 'partials/contact.html'
+        })
+
+        .otherwise({ redirectTo: '/login' });
+}])
+
+.run(['$rootScope', '$location', '$cookieStore', '$http',
+    function ($rootScope, $location, $cookieStore, $http) {
+        // keep user logged in after page refresh
+        $rootScope.globals = $cookieStore.get('globals') || {};
+        if ($rootScope.globals.currentUser) {
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+        }
+
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            // redirect to login page if not logged in
+            if ($location.path() !== '/login' && !$rootScope.globals.currentUser) {
+                $location.path('/login');
+            }
+        });
+    }]);
+
+angular.module('WebApp')
+    .directive('appmenu', function(){
+        return {
+            templateUrl: 'layout/menu.html'
+        };
+    });
---
+++ js/controllers/login.js
@@ -0,0 +1,11 @@
+//var app = angular.module('WebApp', []);
+
+/*
+app.controller('formController', function($scope) {
+	$scope.reset = function(){
+		$scope.login = 'test login';
+		$scope.password = 'test password';
+	}
+	scope.reset();
+});
+*/
