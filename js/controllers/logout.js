1ed09587ac889990c260971ee2840af310054e1a
Epsichaos <constant.camille.chaos@gmail.com>
2015-06-12 18:35:32 +0100

    work on authentification

---
+++ js/controllers/logout.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('WebApp')
+.controller('LogoutCtrl', ['$rootScope', '$scope', '$location', 'Auth', function($rootScope, $scope, $location, Auth) {
+    $scope.user = Auth.user;
+    $scope.userRoles = Auth.userRoles;
+    $scope.accessLevels = Auth.accessLevels;
+
+    $scope.logout = function() {
+        Auth.logout(function() {
+            $location.path('/login');
+        }, function() {
+            $rootScope.error = "Failed to logout";
+        });
+    };
+}]);
