90ea608727d8ed18ab3df884b47ce8b45425f6bd
Epsichaos <constant.camille.chaos@gmail.com>
2015-07-09 12:57:55 +0100

    minor change for sending messages

--- js/controllers/contact.js
+++ js/controllers/contact.js
@@ -52,10 +52,95 @@ app.controller('ContactCtrl', function($rootScope, $scope, $location, $window, s
 		});
 	})
     $scope.modalWarningNoWater = function() {
-        console.log('no water');
+
+        var data_message = {"type": "nowater", "jwt": token};
+        $.ajax({
+            type: 'POST',
+            url: 'http://131.251.176.109:8080/consumer/message',
+            crossDomain: true,
+            data: data_message,
+            success: function (data) {
+                if(data.access == 'granted') {
+                    console.log(data);
+                    bootbox.alert("Message sent !");
+                }
+                else if(data.exception=='signature') {
+                    bootbox.alert("Wrong token signature !");
+                    Auth.logout(function() {
+                        $state.go('public.login');
+                    });
+                }
+                // token expired
+                else if(data.exception=='time') {
+                    bootbox.alert("Expired token !");
+                    Auth.logout(function() {
+                        $state.go('public.login');
+                    });
+                }
+                // unsupported token
+                else if(data.exception=='unsupported') {
+                    bootbox.alert("Unsupported token !");
+                    Auth.logout(function() {
+                        $state.go('public.login');
+                    });
+                }
+                // unknown error
+                else {
+                    bootbox.alert("Unknown error !");
+                    Auth.logout(function() {
+                        $state.go('public.login');
+                    });
+                }
+            },
+            error: function (err) {
+                console.log('Error in POST login request');
+            }
+        });
     }
     $scope.modalWarningColor = function() {
-        console.log('discolored');
+        var data_message = {"type": "discolored", "jwt": token};
+        $.ajax({
+            type: 'POST',
+            url: 'http://131.251.176.109:8080/consumer/message',
+            crossDomain: true,
+            data: data_message,
+            success: function (data) {
+                if(data.access == 'granted') {
+                    console.log(data);
+                    bootbox.alert("Message sent !");
+                }
+                else if(data.exception=='signature') {
+                    bootbox.alert("Wrong token signature !");
+                    Auth.logout(function() {
+                        $state.go('public.login');
+                    });
+                }
+                // token expired
+                else if(data.exception=='time') {
+                    bootbox.alert("Expired token !");
+                    Auth.logout(function() {
+                        $state.go('public.login');
+                    });
+                }
+                // unsupported token
+                else if(data.exception=='unsupported') {
+                    bootbox.alert("Unsupported token !");
+                    Auth.logout(function() {
+                        $state.go('public.login');
+                    });
+                }
+                // unknown error
+                else {
+                    bootbox.alert("Unknown error !");
+                    Auth.logout(function() {
+                        $state.go('public.login');
+                    });
+                }
+            },
+            error: function (err) {
+                console.log('Error in POST login request');
+            }
+        });
     }
     $scope.modalWarningMessage = function() {
         $scope.jwt = token;
